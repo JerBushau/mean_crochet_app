d1be1a9573e9975963553e1d7cdb5871b6853741
Jeremiah Bushau <exjerlock@hotmail.com>
2017-01-27 15:16:50 -0500

    attempting to implement angular-ui-bootstrap

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const angular = require('angular');
+const bootstrap = require('angular-ui-bootstrap')
 
-angular.module('crochetApp', []);
+angular.module('crochetApp', [bootstrap]);
 
 require('./scripts/services');
 require('./scripts/directives');
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,25 +2,27 @@
 
 function MainCtrl($scope, dataService) {
 
-    $scope.getProjects = function() {
-        dataService.getProjects(function(response) {
-            $scope.projects = response.data.projects
-        });
-    }
 
+
+  $scope.getProjects = function() {
+    dataService.getProjects(function(response) {
+        $scope.projects = response.data.projects
+    });
+  }
+
+  $scope.getProjects();
+
+  $scope.addProject = function() {
+    $scope.projects.push({
+      title: $scope.project.title,
+      currentRow: 0,
+      currentStitch: 0
+    });
+
+    dataService.saveProjects($scope.projects);
+    $scope.project.title = '';
     $scope.getProjects();
-
-    $scope.addProject = function(project) {
-        $scope.projects.push({
-            title: $scope.project.title,
-            currentRow: 0,
-            currentStitch: 0
-        });
-
-        dataService.saveProjects($scope.projects);
-        $scope.project.title = '';
-        $scope.getProjects();
-    }
+  }
 }
 
 module.exports = MainCtrl;
